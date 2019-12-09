2a07752677b6be5fb27b10ccabf1218bf7012315
Nahiyan Naim <nahiyannaim@yahoo.com>
2019-12-08 23:04:06 -0600

    Keep styles together

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.css";
 import Header from "./components/Header";
 import Box from "./components/Box";
 import Dialog from "./components/Dialog";
